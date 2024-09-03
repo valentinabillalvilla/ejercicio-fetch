2c6a6f2bdd6e9f146d00ae62374b0507a40284a0
valentinabillalvilla <valentinabillalvilla@gmail.com>
2024-09-03 19:52:59 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,3 +19,14 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+function mostrarContenido() {
+  fetch(DATA_URL, {
+    method: "GET"
+  })
+    .then(response => response.json())
+    .then(data => {
+      showData(data.students);
+    })
+    .catch(error => console.error('Error al cargar la información.', error));
+}
+mostrarContenido();
